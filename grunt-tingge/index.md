a59a3c10b62618cbc40f25dd4765a697e1c1ff98
TingGe <505253293@163.com>
2016-04-26 12:08:52 +0800

    + 添加 javascript标准教程参考

--- grunt-tingge/index.md
+++ grunt-tingge/index.md
@@ -1,11 +1,8 @@
-# 项目管理／JavaScript技术／在线广告行业
-
-1. [react-test](http://tingge.github.io/lab/react-test/index.html)
-2. [angular-test](http://tingge.github.io/lab/angular-test/index.html)
-3. [d3-test](http://tingge.github.io/lab/d3-test/index.html)
-4. [meteor-test](http://tingge.github.io/lab/meteor-test/index.html)
-5. [vue-test](http://tingge.github.io/lab/vue-test/index.html)
-6. [grunt-babel-test](https://tingge.github.io/lab/grunt-babel-test/dist/index.html)
-7. [phantomjs-test](https://github.com/TingGe/tingge.github.io/tree/master/lab/phantomjs-test)
-8. [nightmare-test](https://github.com/TingGe/tingge.github.io/tree/master/lab/nightmare-test)
-
+1. [Github 中最流行的 JavaScript 项目（react-test）](http://tingge.github.io/lab/react-test/index.html)
+2. [我的书柜（angular-test）](http://tingge.github.io/lab/angular-test/index.html)
+3. [JavaScript应用领域（d3-test）](http://tingge.github.io/lab/d3-test/index.html)
+4. [Hacker news(vue-test)](http://tingge.github.io/lab/vue-test/index.html)
+5. [grunt-babel-test](https://tingge.github.io/lab/grunt-babel-test/dist/index.html)
+6. [phantomjs-test](https://github.com/TingGe/tingge.github.io/tree/master/lab/phantomjs-test)
+7. [nightmare-test](https://github.com/TingGe/tingge.github.io/tree/master/lab/nightmare-test)
+8. [meteor-test](http://tingge.github.io/lab/meteor-test/index.html)
--- lab/angular-test/app/scripts/app.js
+++ lab/angular-test/app/scripts/app.js
@@ -1,26 +1,31 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('BookCtrl', function BookCtrl($scope) {
-    $scope.books = [{
-        'name': 'ECMAScript 6入门',
-        'author': '阮一峰',
-        'url': 'http://es6.ruanyifeng.com/'
+  $scope.books = [{
+    'name': 'JavaScript 标准参考教程（alpha）',
+    'author': '阮一峰',
+    'url': 'http://javascript.ruanyifeng.com/'
 
-    }, {
-        'name': 'Babel 入门教程',
-        'author': '阮一峰',
-        'url': 'http://www.ruanyifeng.com/blog/2016/01/babel.html'
-    }, {
-        'name': 'Using ES6 and ES7 in the Browser, with Babel 6 and Webpack',
-        'author': 'CenturyGuo',
-        'url': 'http://www.jianshu.com/p/6969b8822630'
-    }, {
-        'name': 'Use ES6 With Babel6 in Nodejs',
-        'author': 'XGHeaven',
-        'url': 'http://blog.xgheaven.cn/2015/11/13/use-babel6-in-nodejs/'
-    }, {
-        'name': '全栈工程师之路-Node.js',
-        'author': '桑世龙',
-        'url': 'http://i5ting.github.io/nodejs-fullstack/'
-    }]
+  }, {
+    'name': 'ECMAScript 6入门',
+    'author': '阮一峰',
+    'url': 'http://es6.ruanyifeng.com/'
+
+  }, {
+    'name': 'Babel 入门教程',
+    'author': '阮一峰',
+    'url': 'http://www.ruanyifeng.com/blog/2016/01/babel.html'
+  }, {
+    'name': 'Using ES6 and ES7 in the Browser, with Babel 6 and Webpack',
+    'author': 'CenturyGuo',
+    'url': 'http://www.jianshu.com/p/6969b8822630'
+  }, {
+    'name': 'Use ES6 With Babel6 in Nodejs',
+    'author': 'XGHeaven',
+    'url': 'http://blog.xgheaven.cn/2015/11/13/use-babel6-in-nodejs/'
+  }, {
+    'name': '全栈工程师之路-Node.js',
+    'author': '桑世龙',
+    'url': 'http://i5ting.github.io/nodejs-fullstack/'
+  }]
 });
