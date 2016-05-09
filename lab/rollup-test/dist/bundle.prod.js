6106e94561aba51eabc7edc30f95c5a1205a4d20
TingGe <505253293@163.com>
2016-05-09 14:27:25 +0800

    + added lab/rollup-test/

---
+++ lab/rollup-test/dist/bundle.prod.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var version = "1.0.0";
+
+var message = 'current version is ' + version;
+var foo = (function () {
+  return message;
+})
+
+function main () {
+  console.log(foo);
+}
+
+module.exports = main;
---
+++ lab/rollup-test/rollup.config.dev.js
@@ -0,0 +1,10 @@
+import json from 'rollup-plugin-json';
+import babel from 'rollup-plugin-babel'
+
+export default {
+  entry: 'src/main.js',
+  format: 'cjs',
+  plugins: [json(), babel()],
+  dest: 'dist/bundle.dev.js'
+};
+
