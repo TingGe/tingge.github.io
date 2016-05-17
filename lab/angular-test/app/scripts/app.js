80356a8e3551498574b6eb2b7e3f8aa2f06495a7
TingGe <505253293@163.com>
2016-05-17 11:50:25 +0800

    + added makefile

--- lab/angular-test/app/scripts/app.js
+++ lab/angular-test/app/scripts/app.js
@@ -2,6 +2,10 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('BookCtrl', function BookCtrl($scope) {
   $scope.books = [{
+    'name': '跟我一起写Makefile (PDF重制版)',
+    'author': 'SeisMan',
+    'url': 'http://how-to-write-makefile.readthedocs.io/zh_CN/latest/'
+  },{
     'name': '跟我一起写Makefile',
     'author': '',
     'url': 'https://wiki.ubuntu.org.cn/%E8%B7%9F%E6%88%91%E4%B8%80%E8%B5%B7%E5%86%99Makefile'
