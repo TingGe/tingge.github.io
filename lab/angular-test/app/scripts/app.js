75ad61dc52efa3b76c862d19b0ae427b08bda32f
TingGe <505253293@163.com>
2016-05-17 11:21:13 +0800

    + added makefile

--- lab/angular-test/app/scripts/app.js
+++ lab/angular-test/app/scripts/app.js
@@ -2,6 +2,10 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('BookCtrl', function BookCtrl($scope) {
   $scope.books = [{
+    'name': '跟我一起写Makefile',
+    'author': '',
+    'url': 'https://wiki.ubuntu.org.cn/%E8%B7%9F%E6%88%91%E4%B8%80%E8%B5%B7%E5%86%99Makefile'
+  }, {
     'name': 'Linux设备驱动开发详解：基于最新的Linux 4.0内核',
     'author': '宋宝华',
     'url': 'http://book.2cto.com/201508/55118.html'
